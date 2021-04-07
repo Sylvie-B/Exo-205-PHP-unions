73b7365e30ae5dbba1105ca63b5a44838a1dfb57
Sylvie-B <unsolo@hotmail.fr>
2021-04-07 15:07:34 +0200

    union union all

--- index.php
+++ index.php
@@ -7,3 +7,46 @@
  * Utilisez UNION ALL pour afficher toutes les données y compris les doublons, affichez le résultat  à l'aide d'une boucle ou d'un print_r.
  * PS: Si vous utilisez un print_r, alors utilisez la balise <pre> pour un résultat plus propre.
  */
+
+$server = 'localhost';
+$database = 'exo205';
+$user = 'root';
+$pass = '';
+
+try {
+    $conn = new PDO("mysql:host=$server;dbname=$database;charset=utf8", $user, $pass);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $exception) {
+    echo "data base connexion error : " . $exception->getMessage();
+    return null;
+}
+
+$display = $conn->prepare("
+    SELECT username FROM user
+    UNION
+    SELECT username FROM admin
+    UNION
+    SELECT username FROM client
+");
+
+$display->execute();
+
+echo "<pre>";
+print_r($display->fetchAll());
+echo "</pre>";
+
+$dis2 = $conn->prepare("
+    SELECT username FROM user
+    UNION ALL 
+    SELECT username FROM admin
+    UNION ALL 
+    SELECT username FROM client
+");
+
+$dis2->execute();
+
+echo "<pre>";
+print_r($dis2->fetchAll());
+echo "</pre>";
